8c43a29991381c08086b849f5c6597b1bcaa81b3
chinmaydr <chinmaydramamurthy@gmail.com>
2023-05-31 15:22:07 -0700

    vihfisgiwvavohwafvoshab osavgbeuwavgso8gvhoasgfvw

--- assets/js/projectilemotion.js
+++ assets/js/projectilemotion.js
@@ -1,3 +1,4 @@
+/*
 // Define the variables.
 
 const canvas = document.getElementById("canvas1");
@@ -6,11 +7,11 @@ const ctx = canvas.getContext("2d");
 let velocity = 10;
 let angle = 45;
 let inertia = 0;
-let accelerationY = 0.2;
+let accelerationY = 9.8;
 
 let projectileRadius = 10;
-let projectileX = projectileRadius + 5;
-let projectileY = projectileRadius + 50;
+let projectileX = 0;
+let projectileY = canvas.height - projectileRadius;
 
 let speedScale = 0.5;
 let angleScale = 0.1;
@@ -24,9 +25,7 @@ let points = 0;
 
 let won = false;
 
-// console.log(projectileY);
-
-window.onload = function() {
+window.onload = function () {
   ctx.beginPath();
   ctx.arc(projectileX, projectileY, projectileRadius, 0, Math.PI * 2);
   ctx.fillStyle = "red";
@@ -46,30 +45,28 @@ function resetAllVariables() {
 
   projectileRadius = 10;
   projectileX = projectileRadius + 5;
-  projectileY = projectileRadius + 50;
+  projectileY = canvas.height - projectileRadius;
 
   speedScale = 0.5;
   angleScale = 0.1;
   barHeight = randomHeight;
 }
 
-// Function calling is necessary for the event listeners to update input labels in real time.
 function updateVelocityLabel() {
-  velocity = document.getElementById('velocity').value;
-  document.getElementById('velocityLabel').innerHTML = "Velocity: " + velocity;
+  velocity = document.getElementById("velocity").value;
+  document.getElementById("velocityLabel").innerHTML = "Velocity: " + velocity;
 }
 
-// Same thing for the angle.
 function updateAngleLabel() {
-  angle = document.getElementById('angle').value;
-  document.getElementById('angleLabel').innerHTML = "Angle: " + angle;
+  angle = document.getElementById("angle").value;
+  document.getElementById("angleLabel").innerHTML = "Angle: " + angle;
 }
 
-// The event listeners mentioned.
-document.getElementById('velocity').addEventListener('input', updateVelocityLabel);
-document.getElementById('angle').addEventListener('input', updateAngleLabel);
+document
+  .getElementById("velocity")
+  .addEventListener("input", updateVelocityLabel);
+document.getElementById("angle").addEventListener("input", updateAngleLabel);
 
-// Check if user's inputs were collected. All the cool devs use inspect element.
 function getUserGuess() {
   console.log(velocity);
   console.log(angle);
@@ -79,6 +76,8 @@ function getUserGuess() {
 }
 
 function drawProjectile(x) {
+  const angleInRadians = (angle * Math.PI) / 180;
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   inertia += accelerationY;
@@ -90,43 +89,163 @@ function drawProjectile(x) {
   ctx.fill();
   ctx.closePath();
 
-  barHeight = randomHeight; // Update barHeight with the changing projectileY value
+  barHeight = randomHeight;
 
   ctx.fillStyle = "white";
   ctx.fillRect(canvas.width - 100, barHeight, 100, 25);
 
-  x += velocity * speedScale;
+  const velocityX = velocity * speedScale * Math.cos(angleInRadians);
+  const velocityY = velocity * speedScale * Math.sin(angleInRadians);
 
-  if (projectileY + projectileRadius > canvas.height) {
+  x += velocityX;
+  projectileY -= velocityY;
+
+  if (projectileY + projectileRadius >= canvas.height) {
     projectileY = canvas.height - projectileRadius;
-    inertia *= -1;
-  }
+    inertia = 0;
+    velocity = 0;
 
-  if (x + projectileRadius >= canvas.width - 100) {
     if (
-      Math.abs(projectileY + projectileRadius - barHeight) < tolerance ||
-      Math.abs(projectileY + projectileRadius - (canvas.height - barHeight + 25)) < tolerance
+      Math.abs(x + projectileRadius - (canvas.width - 100)) < tolerance &&
+      Math.abs(projectileY + projectileRadius - barHeight) < tolerance
     ) {
       won = true;
-      document.getElementById('finishLine').innerHTML = "You Won! :)"
+      document.getElementById("finishLine").innerHTML = "You Won! :)";
       points += 1;
+    } else {
+      document.getElementById("finishLine").innerHTML = "You Lost :(";
+      won = false; // Set won to false to handle cases when the ball leaves the canvas
     }
   }
-  
+
   if (x + projectileRadius <= canvas.width) {
     requestAnimationFrame(() => drawProjectile(x));
   } else {
-    if (won == false) {
-      document.getElementById('finishLine').innerHTML = "You lost :("
+    if (!won) {
+      document.getElementById("finishLine").innerHTML = "You Lost :(";
     }
   }
-  
 }
 
-/*
-
-The victory checking doesn't work right now. Ask ChatGPT when you come back.
-
-Edit this file so that in the backend server, points doesn't get updated with a value, but rather with a "points += 1" alternative. That will update the user's score.
-
 */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+let velocity = 10;
+let angle = 45;
+let inertia = 0;
+let accelerationY = 9.8;
+
+let projectileRadius = 10;
+let projectileX = 0;
+let projectileY = canvas.height - projectileRadius;
+
+let speedScale = 0.5;
+let angleScale = 0.1;
+
+let randomHeight = Math.random() * (200 - 50) + 50;
+let barHeight = randomHeight;
+
+const tolerance = 5;
+
+let points = 0;
+let won = false;
+
+window.onload = function () {
+  ctx.beginPath();
+  ctx.arc(projectileX, projectileY, projectileRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(canvas.width - 100, barHeight, 100, 25);
+};
+
+function resetAllVariables() {
+  updateVelocityLabel();
+  updateAngleLabel();
+  inertia = 0;
+  accelerationY = 0.2;
+
+  projectileRadius = 10;
+  projectileX = projectileRadius + 5;
+  projectileY = canvas.height - projectileRadius;
+
+  speedScale = 0.5;
+  angleScale = 0.1;
+}
+
+function updateVelocityLabel() {
+  velocity = document.getElementById("velocity").value;
+  document.getElementById("velocityLabel").innerHTML = "Velocity: " + velocity;
+}
+
+function updateAngleLabel() {
+  angle = document.getElementById("angle").value;
+  document.getElementById("angleLabel").innerHTML = "Angle: " + angle;
+}
+
+document
+  .getElementById("velocity")
+  .addEventListener("input", updateVelocityLabel);
+document.getElementById("angle").addEventListener("input", updateAngleLabel);
+
+function getUserGuess() {
+  console.log(velocity);
+  console.log(angle);
+  projectileX = projectileRadius + 5;
+  resetAllVariables();
+  drawProjectile(projectileX);
+}
+
+function drawProjectile(x) {
+  const angleInRadians = (angle * Math.PI) / 180;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  inertia += accelerationY;
+  projectileY += inertia;
+
+  ctx.beginPath();
+  ctx.arc(x, projectileY, projectileRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.fillStyle = "white";
+  ctx.fillRect(canvas.width - 100, barHeight, 100, 25);
+
+  const velocityX = velocity * speedScale * Math.cos(angleInRadians);
+  const velocityY = velocity * speedScale * Math.sin(angleInRadians);
+
+  x += velocityX;
+  projectileY -= velocityY;
+
+  if (projectileY + projectileRadius >= canvas.height) {
+    projectileY = canvas.height - projectileRadius;
+    inertia = 0;
+    velocity = 0;
+
+    if (
+      x + projectileRadius <= canvas.width &&
+      projectileY + projectileRadius >= barHeight &&
+      projectileY - projectileRadius <= barHeight + 25
+    ) {
+      won = true;
+      document.getElementById("finishLine").innerHTML = "You Won! :)";
+      points += 1;
+    } else {
+      document.getElementById("finishLine").innerHTML = "You Lost :(";
+      won = false;
+    }
+  }
+
+  if (x + projectileRadius <= canvas.width) {
+    requestAnimationFrame(() => drawProjectile(x));
+  } else {
+    if (!won) {
+      document.getElementById("finishLine").innerHTML = "You Lost :(";
+    }
+  }
+}
